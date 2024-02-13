87679af683e710f5b14aa7da4231ac38a348c7ba
t0-0 <107523324+t0-0@users.noreply.github.com>
2024-02-13 22:38:36 +0900

    add tile

--- react-2048/src/app/page.tsx
+++ react-2048/src/app/page.tsx
@@ -1,16 +1,37 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+
+const Tile = (props: { num: number }) => {
+  const targetRef: any = useRef(null);
+  const num = props.num;
+  useEffect(() => {
+    if (targetRef.current != null) {
+      console.log(targetRef.current!.getBoundingClientRect()["x"]);
+    }
+  });
+  return (
+    <div className="aspect-square bg-white" ref={targetRef}>
+      <div className="flex justify-center items-center h-full">
+        <div className="text-center text-xl">{num != 0 ? num : ""}</div>
+      </div>
+    </div>
+  );
+};
+
 const Grid = () => {
+  const gridNumber: number = 5;
+  const Tiles = Array(gridNumber * gridNumber).fill(0);
   return (
     <div className="aspect-square bg-teal-200">
-      <div className="grid grid-cols-3 gap-3 p-4">
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
-        <div className="col-span-1 aspect-square bg-white"></div>
+      <div
+        className={`grid ${
+          gridNumber === 3 ? "grid-cols-3" : "grid-cols-5"
+        } gap-3 p-4`}
+      >
+        {Tiles.map((tile) => (
+          <Tile key={tile} num={tile} />
+        ))}
       </div>
     </div>
   );
